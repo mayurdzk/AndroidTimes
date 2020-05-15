a088f719f2d8fd8d8a12c66fc6565626081e808e
mayurdzk <jhonmathews@gmail.com>
2020-05-15 06:39:59 +0530

    Remove bogus  unit tests

--- net/src/test/java/com/mayurdhaka/androidapps/net/ExampleUnitTest.kt
+++ net/src/test/java/com/mayurdhaka/androidapps/net/ExampleUnitTest.kt
@@ -1,21 +1,12 @@
 package com.mayurdhaka.androidapps.net
 
-import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
 
 class ExampleUnitTest {
-	private val feedFetcher: FeedFetcher = FeedFetcher()
     @Test
     fun addition_isCorrect() {
-        runBlocking {
-			val dfFeeds = feedFetcher
-				.getFeedAtURL("https://daringfireball.net/feeds/main")
-			val vergeFeeds = feedFetcher
-				.getFeedAtURL("https://www.theverge.com/rss/index.xml")
-			assertFalse(dfFeeds.isEmpty())
-			assertFalse(vergeFeeds.isEmpty())
-		}
+        assertEquals(2+2, 4)
     }
 }
